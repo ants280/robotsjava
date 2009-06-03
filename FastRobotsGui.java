273e3ae6e9a5c819c4bdf565b871f1332735e7a4
ants280 <jacob.patterson@gmail.com>
2009-06-03 23:37:36 +0000

    Reverted to older version.

---
+++ FastRobotsGui.java
@@ -0,0 +1,24 @@
+/**
+ * A robots game with safe teleports.  On each level completion the player is granted a teleport that will not result in death.  The player can still teleport randomly by pressing '*' even when he/she has available safe teleports.
+ */
+public class FastRobotsGui extends SafeTeleportsGui
+{
+	/**
+	 * Creates a game with fast robots.
+	 *
+	 * @param game The type of game to play.  Must be a typeof SafeTeleportsGame.
+	 */
+	public FastRobotsGui(Game game)
+	{
+		super(game);
+	}
+
+	/**
+	 * Must be implemented from RobotGUI.  Tells what type of Game is being played.
+	 */
+	public String getGameType()
+	{
+		return "Fast robots" + safeTeleportsTitle();
+	}
+}
+
