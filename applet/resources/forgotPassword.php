12a90d4511469e47ebd196ac1c850dcba0f336a9
ants280 <jacob.patterson@gmail.com>
2010-03-11 22:56:17 +0000

    added simple email lost password interface

---
+++ applet/resources/forgotPassword.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ 
+ <head>
+  <title>Example Form/XHTML page</title>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+  <link rel="stylesheet" type="text/css" href="form.css"/>
+  <link rel="stylesheet" type="text/css" href="table.css"/>
+ </head>
+ 
+ <body>
+  <?php
+   //Variable initialization
+   include ('databaseLogin.php');
+   $username = $_POST['username'];
+   $email = $_POST['email'];
+
+   echo "<br/>\n";
+
+   //connect to MySQL
+   $connect = mysql_connect($db_host, $db_user, $db_pwd);
+   if ($connect) { 
+	echo "Database connection sucessful!\n";
+	echo "<br/>\n";
+   }
+   else {
+    die('Could not make a connection to MySQL:<br>'.mysql_error());
+   } 
+
+   //select the database to work with
+   $db_selected = mysql_select_db($database, $connect);
+   if ($db_selected) {
+    //Pass username to game.
+    echo "The database username and password are good\n";
+	echo "<br/>\n";	
+   }
+   else {
+    die('unable to insert into table:<br>'.mysql_error());
+   }
+
+   //make sure the user and the password match.
+   $query = "SELECT username, email FROM ".$table." WHERE username='".$username."' and email='".$email."'";
+   $exists = mysql_query($query);
+   if(mysql_affected_rows() == 0) {
+    die('Incorrect Username/email combination.:<br>'.mysql_error());
+   }
+   else {
+	echo "Mail usename to ".$email."\n";
+   }
+
+   mysql_close($connect);
+  ?>
+ </body>
+</html>
