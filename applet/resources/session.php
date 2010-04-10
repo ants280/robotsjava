0b044db061da24c84b7710a8f6ee01bccb02dbb5
ants280 <jacob.patterson@gmail.com>
2010-04-10 00:52:32 +0000

    testing main bug

--- applet/resources/session.php
+++ applet/resources/session.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
+ session_start();
 
  //Make sure the user has logged in.
- if(!isset($_SESSION['username'])) {
+ if(empty($_SESSION['username'])) {
   die('<meta http-equiv="refresh" content="0, url=denied.php"/>');
  }
 ?>
